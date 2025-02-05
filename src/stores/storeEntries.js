9be65af5fef10e08bf6561dbb2f07203b71b336e
gumrahsindar <gsindar@gmail.com>
2025-02-05 13:47:06 +0300

    Add Supabase dependency and configuration

--- src/stores/storeEntries.js
+++ src/stores/storeEntries.js
@@ -1,165 +1,150 @@
-import { defineStore } from 'pinia'
-import { ref, computed, reactive, watch, nextTick } from 'vue'
-import { uid, Notify, LocalStorage } from 'quasar'
+import { defineStore } from "pinia";
+import { ref, computed, reactive, nextTick } from "vue";
+import { uid, Notify } from "quasar";
 
-export const useStoreEntries = defineStore('entries', () => {
-
+export const useStoreEntries = defineStore("entries", () => {
   /*
     state
   */
-  
-    const entries = ref([
-      // {
-      //   id: 'id1',
-      //   name: 'Salary',
-      //   amount: 4999.99,
-      //   paid: true
-      // },
-      // {
-      //   id: 'id2',
-      //   name: 'Rent',
-      //   amount: -999,
-      //   paid: false
-      // },
-      // {
-      //   id: 'id3',
-      //   name: 'Phone bill',
-      //   amount: -14.99,
-      //   paid: false
-      // },
-      // {
-      //   id: 'id4',
-      //   name: 'Unknown',
-      //   amount: 0,
-      //   paid: false
-      // },
-    ])
 
-    watch(entries.value, () => {
-      saveEntries()
-    })
+  const entries = ref([
+    // {
+    //   id: 'id1',
+    //   name: 'Salary',
+    //   amount: 4999.99,
+    //   paid: true
+    // },
+    // {
+    //   id: 'id2',
+    //   name: 'Rent',
+    //   amount: -999,
+    //   paid: false
+    // },
+    // {
+    //   id: 'id3',
+    //   name: 'Phone bill',
+    //   amount: -14.99,
+    //   paid: false
+    // },
+    // {
+    //   id: 'id4',
+    //   name: 'Unknown',
+    //   amount: 0,
+    //   paid: false
+    // },
+  ]);
 
-    const options = reactive({
-      sort: false
-    })
-
+  const options = reactive({
+    sort: false,
+  });
 
   /*
     getters
   */
-  
-    const balance = computed(() => {
-      return entries.value.reduce((accumulator, { amount }) => {
-        return accumulator + amount
-      }, 0)
-    })
 
-    const balancePaid = computed(() => {
-      return entries.value.reduce((accumulator, { amount, paid }) => {
-        return paid ? accumulator + amount : accumulator
-      }, 0)
-    })
+  const balance = computed(() => {
+    return entries.value.reduce((accumulator, { amount }) => {
+      return accumulator + amount;
+    }, 0);
+  });
 
-    const runningBalances = computed(() => {
-      let runningBalances = [],
-          currentRunningBalance = 0
+  const balancePaid = computed(() => {
+    return entries.value.reduce((accumulator, { amount, paid }) => {
+      return paid ? accumulator + amount : accumulator;
+    }, 0);
+  });
 
-      if (entries.value.length) {
-        entries.value.forEach(entry => {
-          let entryAmount = entry.amount ? entry.amount : 0
-          currentRunningBalance = currentRunningBalance + entryAmount
-          runningBalances.push(currentRunningBalance)
-        })
-      }
+  const runningBalances = computed(() => {
+    let runningBalances = [],
+      currentRunningBalance = 0;
 
-      return runningBalances
-    })
+    if (entries.value.length) {
+      entries.value.forEach((entry) => {
+        let entryAmount = entry.amount ? entry.amount : 0;
+        currentRunningBalance = currentRunningBalance + entryAmount;
+        runningBalances.push(currentRunningBalance);
+      });
+    }
 
+    return runningBalances;
+  });
 
   /*
     actions
   */
-  
-    const addEntry = addEntryForm => {
-      const newEntry = Object.assign({}, addEntryForm, { id: uid(), paid: false })
-      if (newEntry.amount ===  null) newEntry.amount = 0
-      entries.value.push(newEntry)
-    }
+  const loadEntries = () => {
+    console.log("get entries from supabase");
+  };
 
-    const deleteEntry = entryId => {
-      const index = getEntryIndexById(entryId)
-      entries.value.splice(index, 1)
-      removeSlideItemIfExists(entryId)
-      Notify.create({
-        message: 'Entry deleted',
-        position: 'top'
-      })
-    }
+  const addEntry = (addEntryForm) => {
+    const newEntry = Object.assign({}, addEntryForm, {
+      id: uid(),
+      paid: false,
+    });
+    if (newEntry.amount === null) newEntry.amount = 0;
+    entries.value.push(newEntry);
+  };
 
-    const updateEntry = (entryId, updates) => {
-      const index = getEntryIndexById(entryId)
-      Object.assign(entries.value[index], updates)
-    }
+  const deleteEntry = (entryId) => {
+    const index = getEntryIndexById(entryId);
+    entries.value.splice(index, 1);
+    removeSlideItemIfExists(entryId);
+    Notify.create({
+      message: "Entry deleted",
+      position: "top",
+    });
+  };
 
-    const sortEnd = ({ oldIndex, newIndex }) => {
-      const movedEntry = entries.value[oldIndex]
-      entries.value.splice(oldIndex, 1)
-      entries.value.splice(newIndex, 0, movedEntry)
-    }
+  const updateEntry = (entryId, updates) => {
+    const index = getEntryIndexById(entryId);
+    Object.assign(entries.value[index], updates);
+  };
 
-    const saveEntries = () => {
-      LocalStorage.set('entries', entries.value)
-    }
-
-    const loadEntries = () => {
-      const savedEntries = LocalStorage.getItem('entries')
-      if (savedEntries) Object.assign(entries.value, savedEntries)
-    }
-
+  const sortEnd = ({ oldIndex, newIndex }) => {
+    const movedEntry = entries.value[oldIndex];
+    entries.value.splice(oldIndex, 1);
+    entries.value.splice(newIndex, 0, movedEntry);
+  };
 
   /*
     helpers
   */
-  
-    const getEntryIndexById = entryId => {
-      return entries.value.findIndex(entry => entry.id === entryId)
-    }
 
-    const removeSlideItemIfExists = entryId => {
-      // hacky fix: when deleting (after sorting),
-      // sometimes the slide item is not removed
-      // from the dom. this will remove the slide
-      // item from the dom if it still exists
-      // (after entry removed from entries array)
-      nextTick(() => {
-        const slideItem = document.querySelector(`#id-${ entryId }`)
-        if (slideItem) slideItem.remove()
-      })
-    }
+  const getEntryIndexById = (entryId) => {
+    return entries.value.findIndex((entry) => entry.id === entryId);
+  };
 
+  const removeSlideItemIfExists = (entryId) => {
+    // hacky fix: when deleting (after sorting),
+    // sometimes the slide item is not removed
+    // from the dom. this will remove the slide
+    // item from the dom if it still exists
+    // (after entry removed from entries array)
+    nextTick(() => {
+      const slideItem = document.querySelector(`#id-${entryId}`);
+      if (slideItem) slideItem.remove();
+    });
+  };
 
   /*
     return
   */
-  
-    return { 
 
-      // state
-      entries,
-      options,
+  return {
+    // state
+    entries,
+    options,
 
-      // getters
-      balance,
-      balancePaid,
-      runningBalances,
+    // getters
+    balance,
+    balancePaid,
+    runningBalances,
 
-      // actions
-      addEntry,
-      deleteEntry,
-      updateEntry,
-      sortEnd,
-      loadEntries
-
-    }
-    
-})
+    // actions
+    loadEntries,
+    addEntry,
+    deleteEntry,
+    updateEntry,
+    sortEnd,
+  };
+});
