e55979051d27522ab3c9b72c1ff8cd9272cb0ed3
gumrahsindar <gsindar@gmail.com>
2025-02-06 14:45:18 +0300

    Enhance entry management by adding order property, updating order on sort, and fixing Supabase entry reference

--- src/stores/storeEntries.js
+++ src/stores/storeEntries.js
@@ -15,25 +15,29 @@ export const useStoreEntries = defineStore("entries", () => {
     //   id: 'id1',
     //   name: 'Salary',
     //   amount: 4999.99,
-    //   paid: true
+    //   paid: true,
+    // order: 1,
     // },
     // {
     //   id: 'id2',
     //   name: 'Rent',
     //   amount: -999,
-    //   paid: false
+    //   paid: false,
+    // order: 2,
     // },
     // {
     //   id: 'id3',
     //   name: 'Phone bill',
     //   amount: -14.99,
-    //   paid: false
+    //   paid: false,
+    // order: 3,
     // },
     // {
     //   id: 'id4',
     //   name: 'Unknown',
     //   amount: 0,
-    //   paid: false
+    //   paid: false,
+    // order: 4,
     // },
   ]);
 
@@ -80,7 +84,10 @@ export const useStoreEntries = defineStore("entries", () => {
   const loadEntries = async () => {
     entriesLoaded.value = false;
 
-    let { data, error } = await supabase.from("entries").select("*");
+    let { data, error } = await supabase
+      .from("entries")
+      .select("*")
+      .order("order", { ascending: true });
     if (error) useShowErrorMessage(error.message);
     if (data) {
       await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -116,6 +123,7 @@ export const useStoreEntries = defineStore("entries", () => {
   const addEntry = async (addEntryForm) => {
     const newEntry = Object.assign({}, addEntryForm, {
       paid: false,
+      order: generateOrderNumber(),
     });
     if (newEntry.amount === null) newEntry.amount = 0;
     // entries.value.push(newEntry);
@@ -148,7 +156,7 @@ export const useStoreEntries = defineStore("entries", () => {
     Object.assign(entries.value[index], updates);
 
     const { error } = await supabase
-      .from("entriesss")
+      .from("entries")
       .update(updates)
       .eq("id", entryId)
       .select();
@@ -159,15 +167,34 @@ export const useStoreEntries = defineStore("entries", () => {
     }
   };
 
+  const updateEntryOrderNumbers = async () => {
+    const updatedEntries = entries.value.map((entry, index) => {
+      return { id: entry.id, order: index + 1 };
+    });
+
+    const { error } = await supabase
+      .from("entries")
+      .upsert(updatedEntries)
+      .select();
+
+    if (error) useShowErrorMessage(error.message);
+  };
+
   const sortEnd = ({ oldIndex, newIndex }) => {
     const movedEntry = entries.value[oldIndex];
     entries.value.splice(oldIndex, 1);
     entries.value.splice(newIndex, 0, movedEntry);
+    updateEntryOrderNumbers();
   };
 
   /*
     helpers
   */
+
+  const generateOrderNumber = () => {
+    const orderNumbers = entries.value.map((entry) => entry.order);
+    return Math.max(...orderNumbers, 0) + 1;
+  };
 
   const getEntryIndexById = (entryId) => {
     return entries.value.findIndex((entry) => entry.id === entryId);
