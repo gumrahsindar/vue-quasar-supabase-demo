4b37a707afa323b6cb7e0afba31ea1252d9e203a
gumrahsindar <gsindar@gmail.com>
2025-02-07 16:49:55 +0300

    Add router authentication boot file to manage route access based on user authentication state

---
+++ src/boot/router-auth.js
@@ -0,0 +1,18 @@
+import { boot } from "quasar/wrappers";
+import { useStoreAuth } from "src/stores/storeAuth";
+
+export default boot(({ router }) => {
+  router.beforeEach((to) => {
+    const storeAuth = useStoreAuth();
+
+    if (!storeAuth.userDetails.id && to.path !== "/auth") {
+      return "/auth";
+    }
+
+    if (storeAuth.userDetails.id && to.path === "/auth") {
+      return false;
+    }
+
+    return true;
+  });
+});
